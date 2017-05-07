22e5d60646236dcc8fc5f4ed775edf0bb333a348
Sébastien NEDJAR <sebastien@nedjar.com>
2017-05-07 16:15:36 +0200

    Ajout de méthode dans le squelette de l'exercice 4

--- src/main/java/fr/univ_amu/iut/exercice4/Args.java
+++ src/main/java/fr/univ_amu/iut/exercice4/Args.java
@@ -1,31 +1,57 @@
 package fr.univ_amu.iut.exercice4;
 
+import java.util.List;
+
 public class Args {
     public Args(String schema, String[] strings) throws ArgsException {
-
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public int nextArgument() {
-        return 0;
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public boolean getBoolean(char x) {
-        return false;
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public String getString(char x) {
-        return null;
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public boolean has(char x) {
-        return false;
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public int getInt(char x) {
-        return 0;
+        throw new RuntimeException("Not yet implemented !");
     }
 
     public double getDouble(char x) {
-        return 0;
+        throw new RuntimeException("Not yet implemented !");
+    }
+
+    private void parseSchema(String schema) throws ArgsException {
+        throw new RuntimeException("Not yet implemented !");
+    }
+
+    private void parseSchemaElement(String element) throws ArgsException {
+        throw new RuntimeException("Not yet implemented !");
+    }
+
+    private void validateSchemaElementId(char elementId) throws ArgsException {
+        throw new RuntimeException("Not yet implemented !");
+    }
+
+    private void parseArgumentStrings(List<String> argsList) throws ArgsException {
+        throw new RuntimeException("Not yet implemented !");
+    }
+
+    private void parseArgumentCharacters(String argChars) throws ArgsException {
+        throw new RuntimeException("Not yet implemented !");
+    }
+
+    private void parseArgumentCharacter(char argChar) throws ArgsException {
+        throw new RuntimeException("Not yet implemented !");
     }
 }
