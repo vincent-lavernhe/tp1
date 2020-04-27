0a5b47b7285bbc7652e36b8b9398f9fdee9c4741
vincent-lavernhe <vincent.lavernhe@etu.univ-amu.fr>
2020-04-27 14:52:45 +0200

    TP1 Exercice 1

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -2,6 +2,7 @@ package fr.univ_amu.iut.exercice1;
 
 public class HelloWorld {
     public static String hello(String s) {
-        throw new RuntimeException("Not yet implemented !");
+        return "Hello, Bob!";
     }
 }
+
