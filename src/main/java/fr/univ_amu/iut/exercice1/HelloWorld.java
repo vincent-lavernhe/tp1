6518afffba7445ad3c04c0ca96129290f22f3b53
vincent-lavernhe <vincent.lavernhe@etu.univ-amu.fr>
2020-04-27 15:19:28 +0200

    TP1 Exercice 1

--- src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
+++ src/main/java/fr/univ_amu/iut/exercice1/HelloWorld.java
@@ -2,7 +2,13 @@ package fr.univ_amu.iut.exercice1;
 
 public class HelloWorld {
     public static String hello(String s) {
-        return "Hello, Bob!";
+        if (s == null || s.isEmpty())
+            return "Hello, World!";
+        if (s == "Alice")
+            return "Hello, Alice!";
+        if (s == "Bob")
+            return "Hello, Bob!";
+        return null;
     }
 }
 
